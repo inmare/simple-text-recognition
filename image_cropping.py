8b059441a03866d13a2b7782b92c3eba994cbfcb
root <yatsumehole@naver.com>
2022-06-18 07:42:20 +0000

    이미지 분리와 vstack, hstack데이터 추가 기능을 분리

--- image_cropping.py
+++ image_cropping.py
@@ -8,7 +8,8 @@ import graph
 def edges_from_image(image: np.ndarray, top_margin: int, side_margin: int) -> np.ndarray:
     image_w = image.shape[1]
     image_h = image.shape[0]
-    cropped_image = image[top_margin:image_h - side_margin, top_margin:image_w - side_margin]
+    cropped_image = image[top_margin:image_h -
+                          side_margin, top_margin:image_w - side_margin]
 
     return cropped_image
 
@@ -38,48 +39,53 @@ def textbox_from_image(image_file_path: str, textbox_info: dict, denoise_info: d
     min_x, min_y, max_x, max_y = data.get_coords_from_contour(textbox_contour)
 
     padding = textbox_footprint.shape[0] + 20
-    cropped_image = clear_image[min_y - padding:max_y + padding, min_x - padding:max_x + padding]
+    cropped_image = clear_image[min_y - padding:max_y +
+                                padding, min_x - padding:max_x + padding]
 
     return cropped_image
 
 
-def line_from_textbox(textbox_image, line_contour):
+def line_from_textbox(textbox_image: np.ndarray, line_contour: np.ndarray) -> np.ndarray:
+    """
+    텍스트박스에서 줄이 있는 부분을 추출함
+
+    :param textbox_image: numpy array형식의 텍스트박스 이미지
+    :param line_contour: 줄의 contour의 위치를 담은 numpy array
+    :returns: 줄의 이미지를 ndarray형식으로 반환함
+    """
     min_x, min_y, max_x, max_y = data.get_coords_from_contour(line_contour)
-    line_contour_centered = data.fit_contour_to_image(line_contour, min_x, min_y)
+    line_contour_centered = data.fit_contour_to_image(
+        line_contour, min_x, min_y)
     line_image = textbox_image[min_y:max_y + 1, min_x:max_x + 1]
 
-    masked_line_image = process.make_masked_image_from_contour(line_image, line_contour_centered)
+    masked_line_image = process.make_masked_image_from_contour(
+        line_image, line_contour_centered)
 
     return masked_line_image
 
 
-def char_from_chars(chars_image, skeleton_thresh, char_size_dict, min_max_gap):
-    data_len = char_size_dict["data_len"]
-
-    image_data = np.zeros((1, data_len))
+def char_from_chars(chars_image, chars_thresh, min_max_gap):
+    seperated_chars = []
 
     # 정확한 결과를 위해 dilated된 이미지 대신 binary_image를 사용함
-    binary_chars_image = process.make_binary_image(chars_image, thresh_correction=0.0)
-    chars_image_vstack = data.get_image_stack_value(binary_chars_image, mode="v")
-    crop_points = data.get_crop_point_from_vstack(chars_image_vstack, min_max_gap)
+    binary_chars_image = process.make_binary_image(
+        chars_image, thresh_correction=chars_thresh)
+    tmp = binary_chars_image.astype(dtype=int)
+    chars_image_vstack = data.get_image_stack_value(
+        binary_chars_image, mode="v")
+    crop_points = data.get_crop_point_from_vstack(
+        chars_image_vstack, min_max_gap)
 
-    for i in range(len(crop_points)):
+    for i in range(len(crop_points) - 1):
         crop_x_start = crop_points[i]
-        if i != len(crop_points) - 1:
-            crop_x_end = crop_points[i + 1]
-        else:
-            crop_x_end = chars_image.shape[1]
+        crop_x_end = crop_points[i + 1]
 
         # 자르는 지점에 보정값으로 crop_x_end에 1을 추가함
         char_image = chars_image[:, crop_x_start:crop_x_end + 1]
-        char_image = process.move_char_image_to_center(char_image, skeleton_thresh, char_size_dict)
 
-        # vstack, hstack 값 추가
-        char_data = data.add_stack_value_to_image(char_image)
-        image_data = np.append(image_data, char_data[np.newaxis, :], axis=0)
+        seperated_chars.append(char_image)
 
-    image_data = image_data[1:, :]
-    return image_data
+    return seperated_chars
 
 
 if __name__ == "__main__":
